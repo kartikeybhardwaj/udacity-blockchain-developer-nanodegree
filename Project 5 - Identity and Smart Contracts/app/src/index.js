3b77e95ec80d539fb653a7e15f9c33051499b9df
Kartikey Bhardwaj <kartikey.bh21@gmail.com>
2019-06-27 00:05:43 +0530

    implemented lookup functionality

--- Project 5 - Identity and Smart Contracts/app/src/index.js
+++ Project 5 - Identity and Smart Contracts/app/src/index.js
@@ -6,8 +6,10 @@ const App = {
   account: null,
   meta: null,
 
-  start: async function() {
-    const { web3 } = this;
+  start: async function () {
+    const {
+      web3
+    } = this;
 
     try {
       // get contract instance
@@ -26,37 +28,45 @@ const App = {
     }
   },
 
-  setStatus: function(message) {
+  setStatus: function (message) {
     const status = document.getElementById("status");
     status.innerHTML = message;
   },
 
-  createStar: async function() {
-    const { createStar } = this.meta.methods;
+  createStar: async function () {
+    const {
+      createStar
+    } = this.meta.methods;
     const name = document.getElementById("starName").value;
     const id = document.getElementById("starId").value;
-    await createStar(name, id).send({from: this.account});
+    await createStar(name, id).send({
+      from: this.account
+    });
     App.setStatus("New Star Owner is " + this.account + ".");
   },
 
-  // Implement Task 4 Modify the front end of the DAPP
-  lookUp: async function (){
-    
+  lookUp: async function () {
+    // const instance = await StarNotary.deployed();
+    const id = document.getElementById("lookid").value;
+    let name = await lookUptokenIdToStarInfo(id, {
+      from: this.account
+    });
+    App.setStatus("Name of the Star is " + name + ".");
   }
 
 };
 
 window.App = App;
 
-window.addEventListener("load", async function() {
+window.addEventListener("load", async function () {
   if (window.ethereum) {
     // use MetaMask's provider
     App.web3 = new Web3(window.ethereum);
     await window.ethereum.enable(); // get permission to access accounts
   } else {
-    console.warn("No web3 detected. Falling back to http://127.0.0.1:9545. You should remove this fallback when you deploy live",);
+    console.warn("No web3 detected. Falling back to http://127.0.0.1:9545. You should remove this fallback when you deploy live", );
     // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-    App.web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:9545"),);
+    App.web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:9545"), );
   }
 
   App.start();
