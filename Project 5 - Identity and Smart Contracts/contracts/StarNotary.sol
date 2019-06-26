d3a850ecd12866373b4e803cbf685f793cfb8473
Kartikey Bhardwaj <kartikey.bh21@gmail.com>
2019-06-26 20:47:45 +0530

    implemented contract

--- Project 5 - Identity and Smart Contracts/contracts/StarNotary.sol
+++ Project 5 - Identity and Smart Contracts/contracts/StarNotary.sol
@@ -1,4 +1,4 @@
-pragma solidity >=0.4.24;
+pragma solidity >= 0.4 .24;
 
 //Importing openzeppelin-solidity ERC-721 implemented Standard
 import "../node_modules/openzeppelin-solidity/contracts/token/ERC721/ERC721.sol";
@@ -11,17 +11,16 @@ contract StarNotary is ERC721 {
         string name;
     }
 
-    // Implement Task 1 Add a name and symbol properties
     // name: Is a short name to your token
     // symbol: Is a short string like 'USD' -> 'American Dollar'
-    
+    string public constant name = "CryptoStar";
+    string public constant symbol = "USD";
 
     // mapping the Star with the Owner Address
     mapping(uint256 => Star) public tokenIdToStarInfo;
     // mapping the TokenId and price
     mapping(uint256 => uint256) public starsForSale;
 
-    
     // Create Star using the Struct
     function createStar(string memory _name, uint256 _tokenId) public { // Passing the name and tokenId as a parameters
         Star memory newStar = Star(_name); // Star is an struct so we are creating a new Star
@@ -37,11 +36,11 @@ contract StarNotary is ERC721 {
 
 
     // Function that allows you to convert an address into a payable address
-    function _make_payable(address x) internal pure returns (address payable) {
+    function _make_payable(address x) internal pure returns(address payable) {
         return address(uint160(x));
     }
 
-    function buyStar(uint256 _tokenId) public  payable {
+    function buyStar(uint256 _tokenId) public payable {
         require(starsForSale[_tokenId] > 0, "The Star should be up for sale");
         uint256 starCost = starsForSale[_tokenId];
         address ownerAddress = ownerOf(_tokenId);
@@ -49,28 +48,34 @@ contract StarNotary is ERC721 {
         _transferFrom(ownerAddress, msg.sender, _tokenId); // We can't use _addTokenTo or_removeTokenFrom functions, now we have to use _transferFrom
         address payable ownerAddressPayable = _make_payable(ownerAddress); // We need to make this conversion to be able to use transfer() function to transfer ethers
         ownerAddressPayable.transfer(starCost);
-        if(msg.value > starCost) {
+        if (msg.value > starCost) {
             msg.sender.transfer(msg.value - starCost);
         }
     }
 
-    // Implement Task 1 lookUptokenIdToStarInfo
-    function lookUptokenIdToStarInfo (uint _tokenId) public view returns (string memory) {
-        //1. You should return the Star saved in tokenIdToStarInfo mapping
+    // function that returns star saved in tokenIdToStarInfo mapping
+    function lookUptokenIdToStarInfo(uint _tokenId) public view returns(string memory) {
+        return tokenIdToStarInfo[_tokenId].name;
     }
 
-    // Implement Task 1 Exchange Stars function
+    // function that exchange stars between two users
     function exchangeStars(uint256 _tokenId1, uint256 _tokenId2) public {
-        //1. Passing to star tokenId you will need to check if the owner of _tokenId1 or _tokenId2 is the sender
-        //2. You don't have to check for the price of the token (star)
-        //3. Get the owner of the two tokens (ownerOf(_tokenId1), ownerOf(_tokenId1)
-        //4. Use _transferFrom function to exchange the tokens.
+        address owner1 = ownerOf(_tokenId1);
+        address owner2 = ownerOf(_tokenId2);
+        _transferFrom(owner1, owner2, _tokenId1);
+        _transferFrom(owner2, owner1, _tokenId2);
     }
 
-    // Implement Task 1 Transfer Stars
+    // function that transfer star
     function transferStar(address _to1, uint256 _tokenId) public {
-        //1. Check if the sender is the ownerOf(_tokenId)
-        //2. Use the transferFrom(from, to, tokenId); function to transfer the Star
+        address starOwner = ownerOf(_tokenId);
+        require(starOwner == msg.sender, "You can't transfer Star you don't own");
+        transferFrom(starOwner, _to1, _tokenId);
+    }
+
+    // function that returns owner by token id
+    function getOwnerByToken(uint256 _tokenId) public view returns(address) {
+        return ownerOf(_tokenId);
     }
 
 }
