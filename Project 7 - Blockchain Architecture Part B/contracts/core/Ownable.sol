31191263f60aa8de8dc6a9e06ec68bbd72478b31
Kartikey Bhardwaj <kartikey.bh21@gmail.com>
2019-07-01 21:35:13 +0530

    renamed owner to ownerLookup

--- Project 7 - Blockchain Architecture Part B/contracts/core/Ownable.sol
+++ Project 7 - Blockchain Architecture Part B/contracts/core/Ownable.sol
@@ -14,7 +14,7 @@ contract Ownable {
     }
 
     // Look up the address of the owner
-    function owner() public view returns(address) {
+    function ownerLookup() public view returns(address) {
         return origOwner;
     }
 
