5d34ad689391f8f9b7be125358aab20cdb01c790
Kartikey Bhardwaj <kartikey.bh21@gmail.com>
2019-07-09 07:17:25 +0530

    added dapp

--- Project 8 - Advanced Blockchain Concepts and Oracles/src/dapp/contract.js
+++ Project 8 - Advanced Blockchain Concepts and Oracles/src/dapp/contract.js
@@ -1,4 +1,6 @@
 import FlightSuretyApp from '../../build/contracts/FlightSuretyApp.json';
+import FlightSuretyData from '../../build/contracts/FlightSuretyData.json';
+
 import Config from './config.json';
 import Web3 from 'web3';
 
@@ -6,8 +8,10 @@ export default class Contract {
 
     constructor(network, callback) {
         let config = Config[network];
-        this.web3 = new Web3(new Web3.providers.HttpProvider(config.url));
+        // this.web3 = new Web3(new Web3.providers.HttpProvider(config.url));
+        this.web3 = new Web3(new Web3.providers.WebsocketProvider(config.url.replace('http', 'ws')));
         this.flightSuretyApp = new this.web3.eth.Contract(FlightSuretyApp.abi, config.appAddress);
+        this.flightSuretyData = new this.web3.eth.Contract(FlightSuretyData.abi, config.dataAddress);
         this.initialize(callback);
         this.owner = null;
         this.airlines = [];
@@ -37,19 +41,123 @@ export default class Contract {
             }, callback);
     }
 
-    fetchFlightStatus(flight, callback) {
+    isAirline(airlineAccount, callback) {
         let self = this;
-        let payload = {
-            airline: self.airlines[0],
-            flight: flight,
-            timestamp: Math.floor(Date.now() / 1000)
-        }
         self.flightSuretyApp.methods
-            .fetchFlightStatus(payload.airline, payload.flight, payload.timestamp)
+            .isAirline(airlineAccount)
+            .call({
+                from: self.owner
+            }, callback);
+    }
+
+    isAirlineFunded(airlineAccount, callback) {
+        let self = this;
+        self.flightSuretyData.methods
+            .isAirlineFunded(airlineAccount)
+            .call({
+                from: self.owner
+            }, callback);
+    }
+
+    registerAirline(airlineAccount, fromAccount, callback) {
+        let self = this;
+        self.flightSuretyApp.methods
+            .registerAirline(airlineAccount)
+            .send({
+                from: fromAccount,
+                gas: 4000000,
+                gasPrice: 100000000000
+            })
+            .then(this.flightSuretyData.events.evntRegisterAirline({
+                    fromBlock: "latest"
+                },
+                function (error, event) {
+                    callback(error, event.returnValues);
+                }));
+    }
+
+    fund(airlineAccount, callback) {
+        let self = this;
+        self.flightSuretyApp.methods
+            .fund()
+            .send({
+                from: airlineAccount,
+                value: this.web3.utils.toWei("10", "ether"),
+                gas: 4000000,
+                gasPrice: 100000000000
+            }, callback);
+    }
+
+    buy(passengerAccount, airlineAccount, airlineName, timestamp, callback) {
+        let self = this;
+        self.flightSuretyApp.methods
+            .buy(airlineAccount, airlineName, timestamp)
+            .send({
+                from: passengerAccount,
+                value: this.web3.utils.toWei("1", "ether"),
+                gas: 4000000,
+                gasPrice: 100000000000
+            }, callback);
+
+    }
+
+    registerFlight(airlineAccount, airlineName, timestamp, callback) {
+        let self = this
+        self.flightSuretyApp.methods
+            .registerFlight(airlineAccount, airlineName, timestamp)
+            .send({
+                from: airlineAccount,
+                gas: 4000000,
+                gasPrice: 100000000000
+            }, callback);
+    }
+
+    fetchFlightStatus(airlineAccount, airlineName, timestamp, callback) {
+        let self = this;
+        self.flightSuretyApp.methods
+            .fetchFlightStatus(airlineAccount, airlineName, timestamp)
             .send({
                 from: self.owner
             }, (error, result) => {
-                callback(error, payload);
+                callback(error, result);
             });
     }
+
+    async accountBalance(account) {
+        let balance = await this.web3.eth.getBalance(account);
+        return await this.web3.utils.fromWei(balance);
+    }
+
+    checkCredit(passengerAccount, callback) {
+        let self = this;
+        self.flightSuretyApp.methods
+            .checkCredit()
+            .call({
+                    from: passengerAccount,
+                    gas: 4000000,
+                    gasPrice: 100000000000
+                },
+                callback);
+    }
+
+    passengerPaid(passengerAccount, airlineAccount, airlineName, timestamp, callback) {
+        let self = this;
+        self.FlightSuretyData.methods
+            .boughtPassenger(passengerAccount, airlineAccount, airlineName, timestamp)
+            .call({
+                from: passengerAccount,
+                gas: 4000000,
+                gasPrice: 10000000000
+            }, callback);
+    }
+
+    pay(passengerAccount, airlineAccount, airlineName, timestamp, callback) {
+        let self = this;
+        self.flightSuretyApp.methods
+            .pay(passengerAccount, airlineAccount, airlineName, timestamp)
+            .send({
+                from: passengerAccount
+            }, callback);
+    }
+
 }
--- Project 8 - Advanced Blockchain Concepts and Oracles/src/dapp/index.js
+++ Project 8 - Advanced Blockchain Concepts and Oracles/src/dapp/index.js
@@ -5,12 +5,23 @@ import './flightsurety.css';
 (async () => {
 
     let result = null;
+    var Web3 = require("web3");
+    var date = new Date();
+    let registeredAirlines = [];
+    let authorizedAccounts = [];
 
     let contract = new Contract('localhost', () => {
 
+        let flights = {
+            'BC001': [date.getTime() + 1001, contract.airlines[0]],
+            'AD001': [date.getTime() + 2001, contract.airlines[1]],
+            'BC058': [date.getTime() + 3058, contract.airlines[2]],
+            'AD058': [date.getTime() + 4058, contract.airlines[3]],
+            'AD019': [date.getTime() + 5029, contract.airlines[4]]
+        }
+
         // Read transaction
         contract.isOperational((error, result) => {
-            console.log(error, result);
             display('Operational Status', 'Check if contract is operational', [{
                 label: 'Operational Status',
                 error: error,
@@ -18,23 +29,196 @@ import './flightsurety.css';
             }]);
         });
 
-        // User-submitted transaction
-        DOM.elid('submit-oracle').addEventListener('click', () => {
-            let flight = DOM.elid('flight-number').value;
-            // Write transaction
-            contract.fetchFlightStatus(flight, (error, result) => {
-                display('Oracles', 'Trigger oracles', [{
-                    label: 'Fetch Flight Status',
-                    error: error,
-                    value: result.flight + ' ' + result.timestamp
-                }]);
+        // Initialize accounts list
+        authorizedAccounts.push(contract.owner);
+        var optionsAccount = contract.airlines;
+        updateSelectList('selectAirline', contract.airlines);
+        updateSelectList('selectAccount', authorizedAccounts);
+        updateSelectList('populateFlights', Object.keys(flights));
+
+        let flightID = populateFlights.options[populateFlights.selectedIndex].value;
+        const flightTime = document.querySelector('.flight-time');
+        const flightAirline = document.querySelector('.flight-airline');
+        flightTime.textContent = "Flight Time: " + flights[flightID][0];
+        flightAirline.textContent = "Airline: " + flights[flightID][1];
+        updateSelectList('passengerList', contract.passengers);
+        updateSelectList('passengerList2', contract.passengers);
+
+        DOM.elid('register-airline').addEventListener('click', async () => {
+            let caller = selectAccount.options[selectAccount.selectedIndex].value;
+            let airline = selectAirline.options[selectAirline.selectedIndex].value;
+            contract.registerAirline(airline, caller, (error, result) => {
+                let success = result['success'];
+                console.log("Success: " + success);
+                let votes = result['votes'];
+                if (success) {
+                    registeredAirlines.push(airline);
+                    authorizedAccounts.push(airline);
+                    updateList('registeredAirline', registeredAirlines);
+                    updateSelectList('selectAccount', authorizedAccounts);
+                } else if (!success) {
+                    console.log("Needs more votes. Current votes: " + votes);
+                }
             });
+        });
+
+        DOM.elid('populate-registered').addEventListener('click', () => {
+            populateRegistered(contract.airlines, contract);
+        });
+
+        DOM.elid('fund-airline').addEventListener('click', () => {
+            const registered = DOM.elid('populateRegistered');
+            let airline = registered.options[registered.selectedIndex].value;
+            console.log(airline);
+            contract.fund(airline, (error, result) => {
+                console.log(result)
+            });
+        });
+
+        DOM.elid('populate-funded').addEventListener('click', () => {
+            populateFunded(contract.airlines, contract);
+        });
+
+        DOM.elid('populateFlights').addEventListener('change', () => {
+            let flightID = populateFlights.options[populateFlights.selectedIndex].value;
+            const flightTime = document.querySelector('.flight-time');
+            const flightAirline = document.querySelector('.flight-airline');
+            flightTime.textContent = "Flight Time: " + flights[flightID][0];
+            flightAirline.textContent = "Airline: " + flights[flightID][1];
+        });
+
+        DOM.elid('register-flight').addEventListener('click', () => {
+            let flightCode = populateFlights.options[populateFlights.selectedIndex].value;
+            let airline = flights[flightCode][1];
+            let timestamp = flights[flightCode][0];
+            console.log("Registering flight: " + flightCode + " " + airline + " " + timestamp);
+            contract.registerFlight(airline, flightCode, timestamp, (error, result) => {
+                console.log(error);
+            });
+        });
+
+        DOM.elid('buy-insurance').addEventListener('click', () => {
+            let flightCode = populateFlights.options[populateFlights.selectedIndex].value;
+            let airline = flights[flightCode][1];
+            let timestamp = flights[flightCode][0];
+            let passenger = passengerList.options[passengerList.selectedIndex].value;
+            contract.buy(passenger, airline, flightCode, timestamp, (error, result) => {
+                console.log(error);
+            });
+        });
+
+        // User-submitted transaction
+        DOM.elid('submit-oracles').addEventListener('click', () => {
+            let flightCode = populateFlights.options[populateFlights.selectedIndex].value;
+            let airline = flights[flightCode][1];
+            let timestamp = flights[flightCode][0];
+            // Write transaction
+            contract.fetchFlightStatus(airline, flightCode, timestamp, (error, result) => {
+                console.log(error);
+                console.log(result);
+                // display('Oracles', 'Trigger oracles', [ { label: 'Fetch Flight Status', error: error, value: result.flight + ' ' + result.timestamp} ]);
+            });
+        });
+
+        DOM.elid('check-balance').addEventListener('click', async () => {
+            let acc = passengerList2.options[passengerList2.selectedIndex].value;
+            let balance = await contract.accountBalance(acc);
+            let displayDiv = DOM.elid("display-balance");
+            // Fisrt clear 
+            displayDiv.innerHTML = '';
+            let section = DOM.section();
+            section.appendChild(DOM.h5('Balance of passenger ' + acc + ' : ' + balance + ' ETH'));
+            displayDiv.append(section);
+        });
+
+        DOM.elid('check-credits').addEventListener('click', async () => {
+            let acc = passengerList2.options[passengerList2.selectedIndex].value;
+            contract.checkCredit(acc, (error, result) => {
+                let displayDiv = DOM.elid("display-credit");
+                displayDiv.innerHTML = '';
+                let section = DOM.section();
+                section.appendChild(DOM.h5('Credits of passenger ' + acc + ' : ' + result + ' ETH'));
+                displayDiv.append(section);
+            })
+        });
+
+        DOM.elid('check-payment').addEventListener('click', async () => {
+            let acc = passengerList2.options[passengerList2.selectedIndex].value;
+            let flightCode = populateFlights.options[populateFlights.selectedIndex].value;
+            let airline = flights[flightCode][1];
+            let timestamp = flights[flightCode][0];
+            contract.passengerPaid(acc, airline, flightCode, timestamp, (error, result) => {
+                let paidAmount = Web3.utils.fromWei(result.toString(), 'ether')
+                console.log("Passenger has paid " + result)
+            })
+        });
+
+        DOM.elid('withdraw-credits').addEventListener('click', async () => {
+            let acc = passengerList2.options[passengerList2.selectedIndex].value;
+            let flightCode = populateFlights.options[populateFlights.selectedIndex].value;
+            let airline = flights[flightCode][1];
+            let timestamp = flights[flightCode][0];
+            contract.pay(acc, airline, flightCode, timestamp, (error, result) => {
+                console.log(error);
+            })
         });
 
     });
 
 })();
 
+// Updates select list elements
+function updateSelectList(selectId, listElements) {
+    var selectList = DOM.elid(selectId);
+    for (var i = 0; i < listElements.length; i++) {
+        var opt = listElements[i];
+        var el = document.createElement("option");
+        el.textContent = opt;
+        el.value = opt;
+        selectList.appendChild(el);
+    }
+}
+
+// empty data in a list
+function clearSelectList(selectId) {
+    var selectList = DOM.elid(selectId);
+    selectList.innerHTML = '';
+}
+
+
+// Updates list elements
+function updateList(listId, listItem) {
+    var registeredList = DOM.elid(listId);
+    var el = document.createElement("li");
+    el.textContent = listItem;
+    registeredList.appendChild(el);
+}
+
+// get registered airlines
+async function populateRegistered(array, contract) {
+    clearSelectList('populateRegistered');
+    for (const item of array) {
+        await contract.isAirline(item, (error, result) => {
+            if (result) {
+                updateSelectList('populateRegistered', [item])
+            }
+        });
+    }
+}
+
+// get funded airlines
+async function populateFunded(array, contract) {
+    clearSelectList('populateFunded');
+    for (const item of array) {
+        await contract.isAirlineFunded(item, (error, result) => {
+            if (result) {
+                updateSelectList('populateFunded', [item])
+            }
+        });
+    }
+}
+
+// display info
 function display(title, description, results) {
     let displayDiv = DOM.elid("display-wrapper");
     let section = DOM.section();
