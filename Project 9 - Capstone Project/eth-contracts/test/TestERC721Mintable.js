0ba4f59cea89305a8ea8251296aba5b8b448a625
Kartikey Bhardwaj <kartikey.bh21@gmail.com>
2019-07-10 22:18:11 +0530

    added all tests

--- Project 9 - Capstone Project/eth-contracts/test/TestERC721Mintable.js
+++ Project 9 - Capstone Project/eth-contracts/test/TestERC721Mintable.js
@@ -4,31 +4,59 @@ contract('TestERC721Mintable', accounts => {
 
     const account_one = accounts[0];
     const account_two = accounts[1];
+    const account_three = accounts[2];
 
     describe('match erc721 spec', function () {
         beforeEach(async function () {
             this.contract = await ERC721MintableComplete.new({
                 from: account_one
             });
-            // TODO: mint multiple tokens
-        })
+            // mint multiple tokens
+            await this.contract.mint(account_two, 20);
+            await this.contract.mint(account_two, 21);
+            await this.contract.mint(account_two, 22);
+            await this.contract.mint(account_two, 23);
+            await this.contract.mint(account_two, 24);
+            await this.contract.mint(account_two, 25);
+            await this.contract.mint(account_two, 26);
+            await this.contract.mint(account_two, 27);
+            await this.contract.mint(account_two, 28);
+            await this.contract.mint(account_two, 29);
+            await this.contract.mint(account_three, 30);
+            await this.contract.mint(account_three, 31);
+            await this.contract.mint(account_three, 32);
+            await this.contract.mint(account_three, 33);
+            await this.contract.mint(account_three, 34);
+            await this.contract.mint(account_three, 35);
+            await this.contract.mint(account_three, 36);
+            await this.contract.mint(account_three, 37);
+            await this.contract.mint(account_three, 38);
+            await this.contract.mint(account_three, 39);
+        });
 
         it('should return total supply', async function () {
-
-        })
+            const supply = await this.contract.totalSupply();
+            assert.equal(supply, 20, "Incorrect total suppy");
+        });
 
         it('should get token balance', async function () {
-
-        })
+            const accountTwoBalance = await this.contract.balanceOf(account_two);
+            assert.equal(accountTwoBalance, 10, 'Incorrect account balance');
+        });
 
         // token uri should be complete i.e: https://s3-us-west-2.amazonaws.com/udacity-blockchain/capstone/1
         it('should return token uri', async function () {
-
-        })
+            const tokenURI = await this.contract.tokenURI(201);
+            assert.equal(tokenURI, 'https://s3-us-west-2.amazonaws.com/udacity-blockchain/capstone/201', 'Incorrect token uri');
+        });
 
         it('should transfer token from one owner to another', async function () {
-
-        })
+            await this.contract.transferFrom(account_two, account_three, 29, {
+                from: account_two
+            });
+            const newOwner = await this.contract.ownerOf(29);
+            assert.equal(newOwner, account_three, 'Incorrect new owner');
+        });
     });
 
     describe('have ownership properties', function () {
@@ -36,15 +64,24 @@ contract('TestERC721Mintable', accounts => {
             this.contract = await ERC721MintableComplete.new({
                 from: account_one
             });
-        })
+        });
 
         it('should fail when minting when address is not contract owner', async function () {
-
-        })
+            try {
+                await this.contract.mint(account_two, 222, {
+                    from: account_two
+                });
+                return;
+            } catch (error) {
+                assert.exists(error);
+            }
+            assert.fail('Expected an error but didnt see one!');
+        });
 
         it('should return contract owner', async function () {
-
-        })
+            const contractOwner = await this.contract.getOwner();
+            assert.equal(contractOwner, account_one, 'Incorrect contract owner');
+        });
 
     });
-})
+});
